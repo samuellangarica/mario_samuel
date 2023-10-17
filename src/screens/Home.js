05bba1804b188cc2c2ffc80b1b9f9e310d6357a2
MarioRG01 <90581036+MarioRG01@users.noreply.github.com>
2023-10-17 11:47:50 -0600

    ma

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,13 +4,18 @@ import '../styles/Home.css';
 function Home() {
   return (
     <>
-      <h1>
+    <img src='https://sm.ign.com/ign_es/news/u/uncharted-/uncharted-3s-director-reveals-the-origin-of-the-famous-cargo_wxzs.jpg' alt='' />
+    <div className='banner'>
+        <h1>"Welcome!"</h1>
+        <h1> to our</h1>
+        <h1>
         <span>Portfolio</span>
         <div className="message">
           <div className="word1">Samuel</div>
           <div className="word2">Mario</div>
         </div>
       </h1>
+      </div>
     </>
   );
 }
