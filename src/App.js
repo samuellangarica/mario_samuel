7460fbfc39bf74a5d25f51fa696501e703d888ff
samuellangarica <samslangarica@gmail.com>
2023-10-16 18:44:42 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import logo from './logo_up.png';
+import './styles/App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OurProjects from './screens/OurProjects';
-import './App.css';
+import Home from './screens/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="wavy">
-          <span style={{'--i': 1}}>L</span>
-          <span style={{'--i': 2}}>o</span>
-          <span style={{'--i': 3}}>a</span>
-          <span style={{'--i': 4}}>d</span>
-          <span style={{'--i': 5}}>i</span>
-          <span style={{'--i': 6}}>n</span>
-          <span style={{'--i': 7}}>g</span>
-          <span style={{'--i': 8}}>.</span>
-        </div>
-      </header>
-    </div>
+    
+      <div className="App">
+      <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/our-projects" element={<OurProjects />} />
+        </Routes>
+      </div>
+    </Router>
+      </div>
+    
     
   );
 }
 
-export default OurProjects;
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='btn'><Link to="/">Home</Link></div>
+        <div className='btn'><Link to="/our-projects">Our Projects</Link></div>
+    </nav>
+  );
+};
+
+export default Navbar;
