b38023afb81409224d53e358be22f2f9c6cd8bd0
MarioRG01 <90581036+MarioRG01@users.noreply.github.com>
2023-10-17 19:30:22 -0600

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import '../styles/Home.css'; 
+import React from "react";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <>
-    <img src='https://sm.ign.com/ign_es/news/u/uncharted-/uncharted-3s-director-reveals-the-origin-of-the-famous-cargo_wxzs.jpg' alt='' />
-    <div className='banner'>
-        <h1>"Welcome!"</h1>
+      <img
+        className="uwu"
+        src="https://sm.ign.com/ign_es/news/u/uncharted-/uncharted-3s-director-reveals-the-origin-of-the-famous-cargo_wxzs.jpg"
+        alt=""
+      />
+      <div className="banner">
+        <h1>Welcome!</h1>
         <h1> to our</h1>
         <h1>
-        <span>Portfolio</span>
-        <div className="message">
-          <div className="word1">Samuel</div>
-          <div className="word2">Mario</div>
-        </div>
-      </h1>
+          <span>Portfolio</span>
+          <div className="message">
+            <div className="word1">Samuel</div>
+            <div className="word2">Mario</div>
+          </div>
+        </h1>
       </div>
     </>
   );
